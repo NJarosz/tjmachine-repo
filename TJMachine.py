5763d0a0829b4ec78bf299e058c7d7758b8b5206
NJarosz <63024003+NJarosz@users.noreply.github.com>
2021-02-08 11:59:04 -0600

    Update TJMachine.py

--- TJMachine.py
+++ TJMachine.py
@@ -1,51 +1,29 @@
-import RPi.GPIO as GPIO
+from gpiozero import LED, Button, OutputDevice
 from mfrc522 import SimpleMFRC522
 from datetime import date
+import os
 import time
 import csv
 
 # TJ Machine number
 PI_NUM = '01'
-
-# Sets GPIO layout
-GPIO.setmode(GPIO.BCM)
-# Sets up RFID Reader
-reader = SimpleMFRC522()
+    
 # Sets up active GPIO's as variables
-rfid_led = 12
-err_led = 6
-button = 16
-relay1 = 4
-relay2 = 17
-relay3 = 27
-relay4 = 22
+rfid_led = LED(12)
+err_led = LED(5)
+
+relay1 = OutputDevice(4, active_high=False)
+relay2 = OutputDevice(17, active_high=False)
+relay3 = OutputDevice(27, active_high=False)
+relay4 = OutputDevice(22, active_high=False)
 relays = (relay1, relay2, relay3, relay4)
 
+button1 = Button(13, pull_up=True)
 
-# Sets up Buttons
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-# Sets up RFID LED indicator
-GPIO.setup(rfid_led, GPIO.OUT)
-
-# Sets up Error LED
-GPIO.setup(err_led, GPIO.OUT)
-
-# Sets up Relays
-GPIO.setup(relay1, GPIO.OUT)
-GPIO.setup(relay2, GPIO.OUT)
-GPIO.setup(relay3, GPIO.OUT)
-GPIO.setup(relay4, GPIO.OUT)
+# Sets up RFID Reader
+reader = SimpleMFRC522()
 
 
-#Creates functions to control GPIO pins.
-# gpio_high() is OFF for Relays, on for LED
-def gpio_high(gpio):
-    GPIO.output(gpio, GPIO.HIGH)
-# gpio_low() is ON for Relays, off for LED
-def gpio_low(gpio):
-    GPIO.output(gpio, GPIO.LOW)
-    
 def read_main():
     """reads main to determine which
     file to read to create sequence"""
@@ -92,8 +70,13 @@ def create_sequence(filename):
                     sequence = {}
                     part = None
                     mach = None
-                    return part, mach, sequence
     return part, mach, sequence
+
+
+# Instantiates the sequence
+txt_file = read_main()
+filename = "/home/pi/Desktop/" + str(txt_file)
+part_num, mach_num, seq = create_sequence(filename)
 
 
 def evaluate_seq(seq_dict, relays, part, mach):
@@ -103,6 +86,8 @@ def evaluate_seq(seq_dict, relays, part, mach):
     relay number in sequence, error led
     turns on."""
     b = False
+    if part == None or mach == None:
+        return b 
     if len(part) == 0 or len(mach) == 0:
         return b
     if seq_dict:
@@ -120,29 +105,13 @@ def evaluate_seq(seq_dict, relays, part, mach):
     return b
 
 
-def run_sequence(seq_dict, relays):
-    """Uses the dictionary returned by
-    create_sequence() to trigger relays/timers."""
-    try:
-        for key, value in seq_dict.items():
-            if "on" in key:
-                gpio_low(eval(value))
-            elif "tmr" in key:
-                time.sleep(value)
-            elif "off" in key:
-                gpio_high(eval(value))
-        gpio_high(relays)
-    except:
-        gpio_high(relays)
-        
-
 def add_timestamp():
     """opens or creates a csv file with todays date in
     filename. Adds timestamp to that csv including machine
     number, part number, id number, user, time, date"""
     today = date.today()
     now = time.strftime("%H:%M:%S")
-    data = (PI_NUM, mach_num, part_num, id_num, user.strip(), now, today)
+    data = (PI_NUM, mach_num, part_num, id_num, user, now, today)
     path = "/home/pi/Documents/CSV/"
     filename = today.strftime("%Y%m%d") + f"PI{PI_NUM}.csv"
     with open(path + filename, "a", newline="") as fa, \
@@ -155,19 +124,30 @@ def add_timestamp():
             writer.writerow(header)
         writer.writerow(data)
 
+def gpio_on(pin):
+    pin.on()
+    
+def gpio_off(pin):
+    pin.off()
+    
+def run_sequence(seq_dict=seq, relays=relays):
+    """Uses the dictionary returned by
+    create_sequence() to trigger relays/timers."""
+    try:
+        for key, value in seq_dict.items():
+            if "on" in key:
+                gpio_on(eval(value))
+            elif "tmr" in key:
+                time.sleep(value)
+            elif "off" in key:
+                gpio_off(eval(value))
+        for relay in relays:
+            relay.off()
+    except:
+        for relay in relays:
+            relay.off()
 
         
-# Initializes relays to the off positions
-gpio_high(relays)
-# Initializes RFID LED to off
-gpio_low(rfid_led)
-# Initializes Error LED to off
-gpio_low(err_led)
-
-# Instantiates the sequence
-txt_file = read_main()
-filename = "/home/pi/Desktop/" + str(txt_file)
-part_num, mach_num, seq = create_sequence(filename)
 # Evaluates the sequence
 gate = evaluate_seq(seq, relays, part_num, mach_num)
 
@@ -177,31 +157,24 @@ if gate:
 
             # Read info on RFID card, if present
             id_num, user = reader.read()
+            user = user.strip()
 
             if user is not None:
-                gpio_high(rfid_led)
+                rfid_led.on()
 
                 # Waits 7 seconds for button press to trigger relay
-                trigger = GPIO.wait_for_edge(button, GPIO.RISING, timeout=7000)
-                if trigger:
-                    run_sequence(seq, relays)
-                    add_timestamp()
-                elif trigger is None:
-                    pass
-                    
-                gpio_low(rfid_led)
-                user = None
+                if button1.wait_for_press(timeout=7):
+                    if button1.wait_for_release():
+                        run_sequence()
+                        add_timestamp()
+                user = None   
+                rfid_led.off()
+                
 
     except KeyboardInterrupt:
-        GPIO.cleanup()
-        print("exit")
-
-    except Exception as e:
-        GPIO.cleanup()
-        print(e)
-        
+        for relay in relays:
+            relay.off()
+            
 else:
-    gpio_high(err_led)
+    err_led.on()
     time.sleep(10)
-    GPIO.cleanup()
-
