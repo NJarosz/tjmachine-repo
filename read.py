838c9a2ac94e4c8d8427b8bb75d7c45aa490e47c
NJarosz <63024003+NJarosz@users.noreply.github.com>
2021-03-12 11:10:56 -0700

    Update read.py

--- read.py
+++ read.py
@@ -1,2 +1,12 @@
-from MFRC522 import SimpleMFRC522
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
 
+reader = SimpleMFRC522()
+
+
+try:
+    idn, name = reader.read()
+    print("Current Info:, str(idn), name)
+finally:
+    GPIO.cleanup()
+    
