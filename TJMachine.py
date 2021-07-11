cad3ef6996c3556254d9ae027b7f0e6ce7946e39
Nick Laptop <Njarosz1@gmail.com>
2021-07-11 16:00:29 -0600

    commit

--- TJMachine.py
+++ TJMachine.py
@@ -6,30 +6,30 @@ import time
 import csv
 
 # PLC Number
-
 with open("/etc/hostname", "r") as hn:
     pi = hn.readline().rstrip("\n")
     
 # Sets up active GPIO's as variables
 
-lcd = I2C_LCD_driver.lcd()
 relay1 = OutputDevice(4, active_high=False)
 relay2 = OutputDevice(17, active_high=False)
 relay3 = OutputDevice(27, active_high=False)
 relay4 = OutputDevice(22, active_high=False)
 relays = (relay1, relay2, relay3, relay4)
-
 button1 = Button(26, pull_up=True)
 
-# Sets up RFID Reader
-#reader = SimpleMFRC522()
+# Variables/paths
+csv_path = "/home/pi/Documents/CSV/"
+file_path = ""
+count_path = "/home/pi/Documents/totalcount"
+main = "/home/pi/Desktop/main"
+shot = "SHOT"
+lcd = I2C_LCD_driver.lcd()
 
-
-def read_main():
+def read_main(path=main):
     """reads main to determine which
     file to read to create sequence"""
-    main = "/home/pi/Desktop/main"
-    with open(main, 'r') as m:
+    with open(path, 'r') as m:
         txt = m.readline().rstrip("\n")
     return txt
  
@@ -78,8 +78,28 @@ def create_sequence(filename):
 txt_file = read_main()
 filename = "/home/pi/Desktop/" + str(txt_file)
 part_num, mach_num, seq = create_sequence(filename)
-print(part_num, mach_num)
 
+
+def read_count(file=count_path):
+    """Reads/ returns running total part count"""
+    with open(file, "r", newline="") as f:
+        total_count = f.readline()
+        if len(str(total_count)) == 0:
+            total_count = 0
+        else:
+            total_count = int(total_count)
+    return total_count
+
+
+def write_count(part_count, file=count_path):
+    """Writes total part count to totalcount file"""
+    if part_count > 999999:
+        part_count = 0
+    with open(file, "w") as f:
+        f.write(str(part_count))
+
+
+count = read_count()
 
 def evaluate_seq(seq_dict, relays, part, mach):
     """tries to catch any typos in relay 
@@ -106,25 +126,41 @@ def evaluate_seq(seq_dict, relays, part, mach):
                     b = False           
     return b
 
+def create_file_path(day, path=csv_path):
+    """Creates a new file path from today's date and pi name"""
+    filename = day.strftime("%Y%m%d") + f"{pi}.csv"
+    file_path = path + filename
+    return file_path
 
-def add_timestamp():
+def create_csv(file):
+    """creates a new csv file, inserts a header"""
+    with open(file, "a", newline="") as fa, \
+            open(file, "r", newline='') as fr:
+        writer = csv.writer(fa, delimiter=",")
+        line = fr.readline()
+        if not line:  # if CSV is empty, add header
+            header = ("Type", "pi", "Machine", "Part",
+                      "User_ID", "Time", "Date")
+            writer.writerow(header)
+
+def add_timestamp(cat, file):
     """opens or creates a csv file with todays date in
     filename. Adds timestamp to that csv including machine
     number, part number, id number, user, time, date"""
-    today = date.today()
     now = time.strftime("%H:%M:%S")
-    data = (pi, mach_num, part_num, user, now, today)
-    path = "/home/pi/Documents/CSV/"
-    filename = today.strftime("%Y%m%d") + f"{pi}.csv"
-    with open(path + filename, "a", newline="") as fa, \
-            open(path + filename, "r", newline='') as fr:
+    data = (cat, pi, mach_num, part_num, emp_num, now, today)
+    with open(file, "a", newline="") as fa:
         writer = csv.writer(fa, delimiter=",")
-        line = fr.readline()  # check if empty
-        if not line:  # if empty, add header
-            header = ("pi", "Machine", "Part",
-                      "User_ID", "Time", "Date")
-            writer.writerow(header)
         writer.writerow(data)
+
+
+def update_csv():
+    """Updates the CSV file path with Today's date
+    and creates a new csv from that file name"""
+    today = date.today()
+    file_path = create_file_path(day=today)
+    create_csv(file=file_path)
+    return today, file_path
 
 def gpio_on(pin):
     pin.on()
@@ -155,20 +191,27 @@ gate = evaluate_seq(seq, relays, part_num, mach_num)
 
 if gate:
     lcd.clear()
+    today, file_path = update_csv()
+
     try:
         while True:
 
             # Read info on RFID card, if present
             #id_num, user = reader.read()
-            user = "TJ USER"
+            user = "tj user"
             lcd.message(f"{part_num} {mach_num}",1)
             if user != None:
-                
+                lcd.message(f"Cnt: {count}",2)
                 # Waits 7 seconds for button press to trigger relay
                 if button1.is_pressed:
                     button1.wait_for_release()
                     run_sequence()
-                    add_timestamp()
+                    add_timestamp(shot, file_path)
+                    count += 1
+                    write_count(count)
+
+                if date.today() != today:
+                    today, file_path = update_csv()
                 
 
 
