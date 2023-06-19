bcfd5863d19a3780ad2ec4a46e75a71e61858818
njaro <NJarosz1@gmail.com>
2023-06-19 18:49:18 -0500

    major overhaul

--- TJMachine.py
+++ TJMachine.py
@@ -22,11 +22,11 @@ relay2 = OutputDevice(17, active_high=False)
 relay3 = OutputDevice(27, active_high=False)
 relay4 = OutputDevice(22, active_high=False)
 relays = (relay1, relay2, relay3, relay4)
-hand_button = Button(26, pull_up=True, bounce_time=1.5, hold_repeat=False)
-hand_button2 = Button(13, pull_up=True)
+foot_button = Button(26, pull_up=True)
+eyebeam = Button(13, pull_up=True)
 gr_button = Button(16, pull_up=True, hold_time=2)
 red_button = Button(12, pull_up=True, hold_time=1)
-rfid_bypass = Button (23, pull_up=True)
+rfid_bypass = Button(23, pull_up=True)
 reader = SimpleMFRC522()
 lcd = I2C_LCD_driver.lcd()
 
@@ -34,7 +34,8 @@ lcd = I2C_LCD_driver.lcd()
 # Variables/paths
 csv_path = "/home/pi/Documents/CSV/"
 count_pkl = "/home/pi/Documents/counts.pickle"
-prod_vars_pkl = "/home/pi/Documents/vars.pickle"
+production_info = "/home/pi/production_info.txt"
+employee_info = "/home/pi/employee_info.txt"
 main = "/home/pi/Desktop/main"
 file_path = ""
 logon = "LOG_ON"
@@ -48,6 +49,7 @@ modes = {"setup": 0,
          }
 mode = modes["standby"]
 startup = True
+employees = {}
 invalid_msg = "Invalid Data"
 invalid_seq = "Invalid Seq"
 menu_msg_top = "Reset Counter?"
@@ -58,12 +60,6 @@ timeoutm = "Timed Out"
 db_name = "device_vars"
 
 
-try:
-    db_host = os.environ.get("DB_HOST_1")
-    db_user = os.environ.get("DB_USER_1")
-    db_psw = os.environ.get("DB_PSW_1")
-except:
-    pass
 
 # Functions
 
@@ -78,59 +74,51 @@ def save_vars(dict, pkl_file):
     with open(pkl_file, "wb") as pckl:
         pickle.dump(dict, pckl)
 
-prod_vars_dict = read_pckl_counts(prod_vars_pkl)
+def read_production_info(filename):
+    """Reads the part, machine and count goal info from the production info text file
+    saved in Documents.  Returns each value, and returns dummy values if not valid"""
+    with open(filename, 'r') as text:
+        for line in text:
+            if len(line.strip()) == 0:      #skips any blank lines
+                pass
+            elif "#" in line:
+                pass
+            else:
+                try:
+                    key, value = line.replace(' ','').strip().split(",")
+                    key = key.lower()
+                    if key == "part":
+                        part = value
+                    elif key == "mach":
+                        machine = value
+                    elif key == "count_goal":
+                        count_goal = int(value)
+                except:
+                    part = "999"
+                    machine = "UNK"
+                    count_goal = 0
 
-def read_machvars_db(count_num=count_num):
-    # Attempts to pull current production variables from DB, or else from Pickle Files
-    try:
-        conn = mysql.connector.connect(
-            host=db_host,
-            user=db_user,
-            passwd=db_psw,
-            database=db_name
-        )
-        c = conn.cursor()
-        c.execute("SELECT part FROM data_vars WHERE device_id=%s", (count_num,))
-        part = c.fetchone()
-        part = str(part[0])
+    return part, machine, count_goal
 
-        c.execute("SELECT machine FROM data_vars WHERE device_id=%s", (count_num,))
-        mach = c.fetchone()
-        mach = str(mach[0])
-
-        c.execute("SELECT count_goal FROM data_vars WHERE device_id = %s", (count_num,))
-        countset = c.fetchone()
-        countset = int(countset[0])
-        c.close()
-    except:
-        try:
-            part = str(prod_vars_dict['part'])
-            mach = str(prod_vars_dict['mach'])
-            countset = int(prod_vars_dict['countset'])
-        except:
-            part = 9999
-            mach = 99
-            countset = 0
-
-    return part, mach, countset
+part_num, mach_num, count_goal = read_production_info()
     
-def ret_emp_name(id_num):
+def ret_emp_names(filename):
     # Attempts to retrieve employee name from DB based on the number on their ID card
-    try:
-        conn = mysql.connector.connect(
-            host=db_host,
-            user=db_user,
-            passwd=db_psw,
-            database=db_name
-        )
-        c = conn.cursor()
-        c.execute("SELECT name FROM employees WHERE emp_id=%s", (id_num,))
-        emp_name=c.fetchone()
-        emp_name=str(emp_name[0])
-        c.close()
-        return emp_name
-    except:
-        return None
+    emps = {}
+    with open(filename, 'r') as text:
+        for line in text:
+            if len(line.strip()) == 0:  # skips any blank lines
+                pass
+            elif "#" in line:
+                pass
+            else:
+                key, value = line.replace(' ', '').strip().split(",")
+                key = key.lower()
+                emps[int(key)] = value
+
+    return emps
+
+employees = ret_emp_names(employee_info)
 
 def read_main(path=main):
     """reads main to determine which
@@ -291,14 +279,14 @@ def evaluate_seq(seq_dict, relays):
 def invalid_sequence():
     lcd.clear()
     lcd.message(invalid_seq)
-    time.sleep(30)
+    time.sleep(60)
     lcd.clear()
 
 
 def invalid_params():
     lcd.clear()
     lcd.message(invalid_msg)
-    time.sleep(30)
+    time.sleep(60)
     lcd.clear()
 
 def gpio_on(pin):
@@ -337,12 +325,10 @@ try:
             txt_file = read_main()
             filename = "/home/pi/Desktop/" + str(txt_file)
             seq = create_sequence(filename)
-            part_num, mach_num, countset = read_machvars_db()
-            param_test, prod_vars_dict = evaluate_params(part_num, mach_num, countset, prod_vars_dict)
+            param_test = True #, prod_vars_dict = evaluate_params(part_num, mach_num, countset, prod_vars_dict)
             sequence_test = evaluate_seq(seq, relays)
             if param_test is True:
                 if sequence_test is True:
-                    save_vars(prod_vars_dict, prod_vars_pkl)
                     lcd.clear()
                     standby_info_top = f"Part:{part_num}"
                     standby_info_btm = f"Cnt:{total_count} Mch:{mach_num}"
@@ -359,8 +345,7 @@ try:
                                 txt_file = read_main()
                                 filename = "/home/pi/Desktop/" + str(txt_file)
                                 seq = create_sequence(filename)
-                                part_num, mach_num, countset = read_machvars_db()
-                                param_test, prod_vars_dict = evaluate_params(part_num, mach_num, countset, prod_vars_dict)
+                                param_test = True #, prod_vars_dict = evaluate_params(part_num, mach_num, countset, prod_vars_dict)
                                 seq_test = evaluate_seq(seq, relays)
                                 if param_test is True:
                                     if seq_test is True:
@@ -376,37 +361,19 @@ try:
                                 red_button.wait_for_release()
                                 time.sleep(0.2)
                                 mode = modes["menu"]
-                        else:    
-                            idn, emp_num = reader.read_no_block()
-                            if emp_num != None:
-                                emp_num = emp_num.strip()
-                                if emp_num == '':
-                                    pass
-                                else:
-                                    emp_name = ret_emp_name(emp_num)
-                                    emp_count = 0
-                                    add_timestamp(logon, file_path)
-                                    mode = modes["run"]    
+                        else:
                             if gr_button.is_pressed:
                                 gr_button.wait_for_release()
-                                txt_file = read_main()
-                                filename = "/home/pi/Desktop/" + str(txt_file)
-                                seq = create_sequence(filename)
-                                part_num, mach_num, countset = read_machvars_db()
-                                param_test, prod_vars_dict = evaluate_params(part_num, mach_num, countset, prod_vars_dict)
-                                seq_test = evaluate_seq(seq, relays)
-                                if param_test is True:
-                                    if seq_test is True:
-                                        save_vars(prod_vars_dict, prod_vars_pkl)
-                                        standby_info_top = f"Part:{part_num}"
-                                        standby_info_btm = f"Cnt:{total_count} Mch:{mach_num}"
-                                        lcd.clear()
-                                        lcd.message(standby_info_top, 1)
-                                        lcd.message(standby_info_btm, 2)
+                                idn, emp_num = reader.read()
+                                if emp_num != None:
+                                    emp_num = emp_num.strip()
+                                    if emp_num == '':
+                                        pass
                                     else:
-                                        invalid_sequence()
-                                else:
-                                    invalid_params()
+                                        emp_name = employees[emp_num]
+                                        emp_count = 0
+                                        add_timestamp(logon, file_path)
+                                        mode = modes["run"]
                             if red_button.is_pressed:
                                 red_button.wait_for_release()
                                 time.sleep(0.2)
@@ -447,22 +414,22 @@ try:
             while mode == modes["run"]:
                 run_msg_btm = f"Cnt:{emp_count}, {total_count}"
                 last_display, last_disp_time = display_run_info(last_display, last_disp_time)
-                if countset == 0:
+                if count_goal == 0:
                     pass
-                elif run_count == countset:
+                elif run_count == count_goal:
                     run_count = count_reset(run_count)
                     button2.wait_for_press()
                     button2.wait_for_release()
                     lcd.clear()
                     lcd.message(run_msg_top2, 1)
                 if datetime.now() >= now + timedelta(seconds=1):
-                    if (hand_button.is_pressed and hand_button2.is_pressed):
+                    if (foot_button.is_pressed and eyebeam.is_pressed):
                         run_sequence()
                         emp_count += 1
                         total_count, run_count = update_counts(total_count, run_count)
                         save_vars(count_dict, count_pkl)
                         add_timestamp(shot, file_path)
-                        hand_button.wait_for_release()
+                        foot_button.wait_for_release()
                         now = datetime.now()
                 if datetime.now() >= now + timedelta(seconds=300):
                     add_timestamp(timeout, file_path)
@@ -480,14 +447,8 @@ except Exception as e:
     lcd.clear()
     lcd.message("ERROR")
     print(e)
-    prod_vars_dict = {"part": 0,
-                  "mach": 0,
-                  "countset": 0}
-
     counts_dict = {"totalcount": 0,
                "runcount": 0}
-
-    save_vars(prod_vars_dict, prod_vars_pkl)
     save_vars(counts_dict, count_pkl)
     time.sleep(5)
     os.system("reboot")
